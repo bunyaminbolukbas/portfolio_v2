478bd0948e12529b7ecbf895a5a9f391637396f7
Bunyamin-1058754 <1058754@hr.nl>
2025-09-22 00:05:12 +0200

    helamaal weggehaad dam ,aar

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Bunyamin Bolukbas',
-  description: 'Portfolio of Bunyamin - Full Stack Developer',
+  
 };
 
 export default function RootLayout({
