c21421a77bb8c35797553a119b68ee756bf58c82
Bunyamin-1058754 <1058754@hr.nl>
2025-09-21 23:58:15 +0200

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,46 +5,9 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Bunyamin Bölükbas',
-  description:
-    'Landingpage of Bünyamin Bölükbas – Full-Stack Developer in Utrecht. Experienced in Next.js, TypeScript, Supabase, and AI-powered software solutions.',
-  keywords: [
-    'Bünyamin Bölükbas',
-    'Bunyamin Bolukbas',
-    'maatwerk software',
-    'maatwerk software ontwikkeling',
-    'maatwerk software oplossingen',
-    'custom software',
-    'custom software development',
-    'Full-Stack Developer',
-    'Next.js',
-    'TypeScript',
-    'Supabase',
-    'Utrecht',
-    'AI software',
-    'Fleet management software',
-    'auto software met ai',
-    'auto software',
-    'AI solutions',
-    'AI-powered applications',
-    'custom software solutions',
-    'bespoke software development',
-    'Performance management software',
-    'software developer',
-    'web developer',
-    'software engineer',
-    'software ontwikkelaar nederland',
-    'web developer nederland',
-    'freelance developer',
-    'freelance software developer',
-    'freelance web developer',
-    'freelance software engineer',
-    'freelance software ontwikkelaar',
-    'freelance web ontwikkelaar',
-    'software development services',
-  ],
+  title: 'Bunyamin Bolukbas',
+  description: 'Portfolio of Bunyamin - Full Stack Developer',
 };
-
 
 export default function RootLayout({
   children,
